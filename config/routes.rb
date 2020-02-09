5b68b9144acccb68aff8077a36cac15ab0f3b319
shinji-2017 <shinji2017@icloud.com>
2020-02-09 22:45:37 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'messages#index'
-  # resources :users ,only: [:show]
+  resources :users 
   resources :messages do
     collection do 
       get 'search'

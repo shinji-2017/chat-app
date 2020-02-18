c60bbd5b0e172870b19e671724d8f42fa53eb8dd
shinji-2017 <shinji2017@icloud.com>
2020-02-18 23:26:24 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,14 +38,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  # def search
-  #   @messages = Message.where(["text LIKE ?", "%#{params[:keyword]}%"])
-  #   respond_to do |format|
-  #     format.html
-  #     format.json
-  #   end
-  # end
-
   private
   def group_params
     params.require(:group).permit( :image, :name, user_ids: [])
@@ -54,5 +46,4 @@ class GroupsController < ApplicationController
   def set_group
     @group = Group.find(params[:id])
   end
-  
 end
